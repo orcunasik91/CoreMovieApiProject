7561cc6d36088f4d949b07e7b7aea3268ee0b676
Orçun Aşık <asikorcun@gmail.com>
2025-02-07 18:05:39 +0300

    Added db context class

---
+++ Infrastructure/CoreMovieApi.Persistence/Context/MovieContext.cs
@@ -0,0 +1,16 @@
+﻿using CoreMovieApi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreMovieApi.Persistence.Context;
+public class MovieContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=127.0.0.1;Initial Catalog=CoreMovie;User ID=sa;Password=Password1;TrustServerCertificate=True;");
+    }
+    public DbSet<Cast> Casts { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Movie> Movies { get; set; }
+    public DbSet<Review> Reviews { get; set; }
+    public DbSet<Tag> Tags { get; set; }
+}
