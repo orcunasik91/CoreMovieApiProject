2cf4e5efd05e8afd919d49fba0e730286a28dcd5
Orçun Aşık <asikorcun@gmail.com>
2025-04-28 21:49:08 +0300

    api consumed for Movie list

--- Frontends/CoreMovieApi.WebUI/Controllers/HomeController.cs
+++ Frontends/CoreMovieApi.WebUI/Controllers/HomeController.cs
@@ -1,10 +1,25 @@
+using CoreMovieApi.WebUI.Dtos.MovieDtos;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CoreMovieApi.WebUI.Controllers;
 public class HomeController : Controller
 {
-    public IActionResult Index()
+    private readonly IHttpClientFactory _httpClientFactory;
+    public HomeController(IHttpClientFactory httpClientFactory)
     {
+        _httpClientFactory = httpClientFactory;
+    }
+    public async Task<IActionResult> Index()
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync("https://localhost:7251/api/Movies/");
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultMovieDto>>(jsonData);
+            return View(values);
+        }
         return View();
     }
 }
