65ade6b44a8adf89a4338c927100367fe6b3c89d
Orçun Aşık <asikorcun@gmail.com>
2025-04-28 21:51:54 +0300

    HttpClient service registered to IoC

--- Frontends/CoreMovieApi.WebUI/Program.cs
+++ Frontends/CoreMovieApi.WebUI/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddHttpClient();
 var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
