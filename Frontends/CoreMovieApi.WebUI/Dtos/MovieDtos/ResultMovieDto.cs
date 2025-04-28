6bde5bd1925ebef515f28d9d17b6d53a6c5099c1
Orçun Aşık <asikorcun@gmail.com>
2025-04-28 21:44:44 +0300

    Created ResultMovieDto to Movie list

---
+++ Frontends/CoreMovieApi.WebUI/Dtos/MovieDtos/ResultMovieDto.cs
@@ -0,0 +1,13 @@
+﻿namespace CoreMovieApi.WebUI.Dtos.MovieDtos;
+public class ResultMovieDto
+{
+    public int MovieId { get; set; }
+    public string Title { get; set; }
+    public string CoverImageUrl { get; set; }
+    public decimal Rating { get; set; }
+    public string Description { get; set; }
+    public int Duration { get; set; }
+    public DateTime ReleaseDate { get; set; }
+    public string CreatedYear { get; set; }
+    public bool IsStatus { get; set; }
+}
